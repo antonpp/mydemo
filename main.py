944fa5ad0ef0b67558139df9b725d57a232252cb
Anton <popovine@google.com>
2023-11-20 11:32:39 +0000

    update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     country = request.args.get("country")
-    return "Welcome to {}!".format(country)
+    return "<h2>Welcome to {}!<h2>".format(country)
 
 if __name__ == "__main__":
     app.run(port=8080)
